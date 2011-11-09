f059700745cc1571e034022c7782c5c72b87a55f
lundberg.mattias@gmail.com <lundberg.mattias@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-09 13:49:11 +0000

    Some changes...

--- trunk/AndroidApplication3/src/com/and/MainActivity.java
+++ trunk/AndroidApplication3/src/com/and/MainActivity.java
@@ -5,7 +5,12 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
@@ -24,38 +29,59 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        
+    }
+
+    private void doIt(BluetoothDevice device) throws FileNotFoundException, IOException {
+        mmSocket = device.createInsecureRfcommSocketToServiceRecord(
+                            MY_UUID_INSECURE);
+        
+        mmSocket.connect();
+        Log.d("BLUETOOTH", "Connected!");
+        OutputStream stream = mmSocket.getOutputStream();
+        
+        stream.write(1); // typ
+        
+        
+        File dir = Environment.getExternalStorageDirectory();
+        File yourFile = new File(dir, "DCIM/.thumbnails/1.jpg");
+        BufferedInputStream buf = new BufferedInputStream(new FileInputStream(yourFile));
+        byte[] buffer = new byte[49];
+        long length = yourFile.length();
+        stream.write(longToBytes(length)); // storlek
+        stream.write(new byte[11]);//{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0});
+        Log.d("Bluetooth", "length" + length);
+        for (int i = 0; i < length; i+=50) {
+            if (i > length) break;
+            
+            long upper = (i+49 > length) ? length - 1 : i+49;
+            //Log.d("bluetooth", "i = " + i + " upper = " + upper + " length = " + length);
+                buf.read(buffer, 0, (int)49);
+                if (i == 0) Log.d("bluetooth", "sending first " + buffer[0] + buffer[1] + buffer[2]);
+                stream.write(buffer);
+            stream.flush();
+        }
+        mmSocket.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         BluetoothDevice device = mBluetoothAdapter.getRemoteDevice("00:1F:E1:EB:3B:DE");
         try {
-            mmSocket = device.createInsecureRfcommSocketToServiceRecord(
-                                MY_UUID_INSECURE);
-            
-            mmSocket.connect();
-            Log.d("BLUETOOTH", "Connected!");
-            OutputStream stream = mmSocket.getOutputStream();
-            
-            stream.write(1); // typ
+            doIt(device);
             
             
-            File dir = Environment.getExternalStorageDirectory();
-            File yourFile = new File(dir, "download/IMG_4750.jpeg");
-            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(yourFile));
-            byte buffer = new byte[49];
-            long length = yourFile.length();
-            stream.write(longToBytes(length)); // storlek
-            stream.write(new byte[11] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0});
-            
-            for (int i = 0, i < length, i+=50) {
-            	buf.read(buffer, i, i+49);
-            	stream.write(buffer);
-            }
-            
-            stream.flush();
             
         } catch (IOException ex) {
             Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
     }
+    
+    
     
     public final byte[] longToBytes(long v) {
         byte[] writeBuffer = new byte[ 8 ];
@@ -68,6 +94,8 @@ public class MainActivity extends Activity
         writeBuffer[5] = (byte)(v >>> 16);
         writeBuffer[6] = (byte)(v >>>  8);
         writeBuffer[7] = (byte)(v >>>  0);
+        
+        Log.d("bluetooth", "Buffer for size! " + writeBuffer.toString());
 
         return writeBuffer;
     }
