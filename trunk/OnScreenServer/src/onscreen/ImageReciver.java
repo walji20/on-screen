f5152cf47cc48c0c4a5a7fc14e620de6d0c91407
lundberg.mattias@gmail.com <lundberg.mattias@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-09 15:41:42 +0000

    Are able to recive and save image correctly...

--- trunk/OnScreenServer/src/onscreen/ImageReciver.java
+++ trunk/OnScreenServer/src/onscreen/ImageReciver.java
@@ -3,8 +3,11 @@ package onscreen;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -50,14 +53,13 @@ public class ImageReciver {
         try {
             result = stream.read(sizeBytes, 0, 8);
         } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+            noti.notify("Failed in reciving name");
         }
-        if (result != 8) {
-            return new File("first result supposed to be 8 is " + result); //TODO: Handle error!
-        }
+
         int size = byteArrayToInt(sizeBytes, 0);
         noti.notify("size is " + size);
         
+        //TODO
         String si = "";
         for (Byte b : sizeBytes) {
             si += b;
@@ -69,12 +71,10 @@ public class ImageReciver {
 
         try {
             int read = stream.read(imageNameByte, 0, 12);
-            if (read != 11 && read != 12) {
-                return new File("first result supposed to be 12 is " + read); //TODO: Handle error!
-            }
         } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+            noti.notify("Failed in reciving bytes");
         }
+        //FIXME
         String imageName = imageNameByte.toString();
         String imageFormat = "jpg";//imageName.split(".")[1];
         
@@ -87,65 +87,85 @@ public class ImageReciver {
         //imageName = imageName.split(".")[0];
         //FIXME 
         imageName = "myimage";
-
-        // recive the image
-        byte[] bytes = new byte[size];
-        try {
-            int read = stream.read(bytes, 0, size);
-            if (read < 1) {
-                return new File("first result supposed to be SIZE is " + size); // TODO: Handle error!
+        
+        String filePath = fileLocation + imageName + "." + imageFormat;
+            File imageFile = new File(filePath);
+            int i = 0;
+            while (imageFile.exists()) {
+                filePath = fileLocation + imageName + i++ + "." + imageFormat;
+                imageFile = new File(filePath);
             }
-        } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+            
+            BufferedOutputStream out = null;
+        try {
+            out = new BufferedOutputStream(new FileOutputStream(imageFile));
+        } catch (FileNotFoundException ex) {
+            noti.notify("Failed in writing/open file");
         }
         
-        String bye = "";
-        for (Byte b : bytes) {
-            bye += b;
+        for(int a = 0; a < size;) {
+
+            // recive the image
+            byte[] bytes = new byte[49];
+            try {
+                int read = stream.read(bytes, 0, 49);
+                if (read == -1) {
+                    out.close();
+                    break;
+                }
+                a+=read;
+                //noti.notify("One recived!");
+            } catch (IOException ex) {
+                noti.notify("Failed in reciving data");
+                
+            }
+
+            
+            try {
+                
+                out.write(bytes);
+                out.flush();
+                
+            } catch (Exception ex) {
+                noti.notify("Failed in writing file");
+            }
         }
-        noti.notify("byte data array = " + bye );
+        //ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+//        Iterator<?> readers = ImageIO.getImageReadersByFormatName(imageFormat);
+//
+//        ImageReader reader = (ImageReader) readers.next();
+//        Object source = bis;
+//
+//        ImageInputStream iis = null;
+//        try {
+//            iis = ImageIO.createImageInputStream(source);
+//        } catch (IOException ex) {
+//            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        reader.setInput(iis, true);
+//        ImageReadParam param = reader.getDefaultReadParam();
+//
+//        Image image = null;
+//        try {
+//            image = reader.read(0, param);
+//        } catch (IOException ex) {
+//            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        BufferedImage bufferedImage =
+//                new BufferedImage(image.getWidth(null),
+//                image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+//        Graphics2D g2 = bufferedImage.createGraphics();
+//        g2.drawImage(image, null, null);
+//
 
-        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-        Iterator<?> readers = ImageIO.getImageReadersByFormatName(imageFormat);
-
-        ImageReader reader = (ImageReader) readers.next();
-        Object source = bis;
-
-        ImageInputStream iis = null;
-        try {
-            iis = ImageIO.createImageInputStream(source);
-        } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        reader.setInput(iis, true);
-        ImageReadParam param = reader.getDefaultReadParam();
-
-        Image image = null;
-        try {
-            image = reader.read(0, param);
-        } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        BufferedImage bufferedImage =
-                new BufferedImage(image.getWidth(null),
-                image.getHeight(null), BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2 = bufferedImage.createGraphics();
-        g2.drawImage(image, null, null);
-
-        String filePath = fileLocation + imageName + "." + imageFormat;
-        File imageFile = new File(filePath);
-        int i = 0;
-        while (imageFile.exists()) {
-            filePath = fileLocation + imageName + i++ + "." + imageFormat;
-            imageFile = new File(filePath);
-        }
-        try {
-            ImageIO.write(bufferedImage, imageFormat, imageFile);
-        } catch (IOException ex) {
-            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
+//        try {
+//            ImageIO.write(bufferedImage, imageFormat, imageFile);
+//        } catch (IOException ex) {
+//            Logger.getLogger(ImageReciver.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         return imageFile;
     }
 }
