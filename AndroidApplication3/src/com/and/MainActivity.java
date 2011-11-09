4c731868e7a4bd84f79468c592ced1d25f1e6daa
kivie89 <kivie89@gmail.com>
2011-11-09 10:45:07 +0000

    Added some code for sending a picture

--- AndroidApplication3/src/com/and/MainActivity.java
+++ AndroidApplication3/src/com/and/MainActivity.java
@@ -34,12 +34,42 @@ public class MainActivity extends Activity
             Log.d("BLUETOOTH", "Connected!");
             OutputStream stream = mmSocket.getOutputStream();
             
-            stream.write(1);
-            stream.write(2);
+            stream.write(1); // typ
+            
+            
+            File dir = Environment.getExternalStorageDirectory();
+            File yourFile = new File(dir, "download/IMG_4750.jpeg");
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(yourFile));
+            byte buffer = new byte[50];
+            long length = yourFile.length();
+            stream.write(longToBytes(length)); // storlek
+            stream.write(new byte[11] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0});
+            
+            for (int i = 0, i < length, i+=50) {
+            	buf.read(buffer, i, i+49);
+            	stream.write(buffer);
+            }
+            
             stream.flush();
             
         } catch (IOException ex) {
             Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    public final byte[] longToBytes(long v) {
+        byte[] writeBuffer = new byte[ 8 ];
+
+        writeBuffer[0] = (byte)(v >>> 56);
+        writeBuffer[1] = (byte)(v >>> 48);
+        writeBuffer[2] = (byte)(v >>> 40);
+        writeBuffer[3] = (byte)(v >>> 32);
+        writeBuffer[4] = (byte)(v >>> 24);
+        writeBuffer[5] = (byte)(v >>> 16);
+        writeBuffer[6] = (byte)(v >>>  8);
+        writeBuffer[7] = (byte)(v >>>  0);
+
+        return writeBuffer;
+    }
+
 }
