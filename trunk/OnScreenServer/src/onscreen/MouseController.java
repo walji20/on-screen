b26c41555c2feafce5bdd23c8c38270da36682b7
john.viklund@gmail.com <john.viklund@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-11 08:50:46 +0000

    Made the mouse controller a thread

--- trunk/OnScreenServer/src/onscreen/MouseController.java
+++ trunk/OnScreenServer/src/onscreen/MouseController.java
@@ -10,71 +10,109 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * 
  * @author Mattias
  */
 public class MouseController {
 
-    private Robot robot = null;
-    private int screenX;
-    private int screenY;
-    private final static byte UP = 1;
-    private final static byte DOWN = 2;
-    private final static byte RIGHT = 3;
-    private final static byte LEFT = 4;
-    private final static byte CENTER = 5;
+	private Robot robot = null;
+	private int screenX;
+	private int screenY;
+	private final static byte UP = 1;
+	private final static byte DOWN = 2;
+	private final static byte RIGHT = 3;
+	private final static byte LEFT = 4;
+	private final static byte CENTER = 5;
+	private MouseThread mouseThread;
 
-    public MouseController() {
-        try {
-            robot = new Robot();
-        } catch (AWTException ex) {
-            Logger.getLogger(MouseController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        screenX = (int) toolkit.getScreenSize().getWidth();
-        screenY = (int) toolkit.getScreenSize().getHeight();
-    }
+	public MouseController() {
+		try {
+			robot = new Robot();
+		} catch (AWTException ex) {
+			Logger.getLogger(MouseController.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+		robot.setAutoDelay(10);
+		mouseThread = new MouseThread(robot);
+		mouseThread.start();
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		screenX = (int) toolkit.getScreenSize().getWidth();
+		screenY = (int) toolkit.getScreenSize().getHeight();
+	}
 
-    public void recive(InputStream stream) {
-        byte[] recivedBytes = new byte[2];
-        try {
-            int result = stream.read(recivedBytes, 0, 2);
-        } catch (IOException ex) {
-            Notification.notify("Some error while reciving.");
-        }
-        int x = MouseInfo.getPointerInfo().getLocation().x;
-        int y = MouseInfo.getPointerInfo().getLocation().y;
+	public void recive(InputStream stream) {
+		byte[] recivedBytes = new byte[2];
+		try {
+			int result = stream.read(recivedBytes, 0, 2);
+		} catch (IOException ex) {
+			Notification.notify("Some error while reciving.");
+		}
+		int x = MouseInfo.getPointerInfo().getLocation().x;
+		int y = MouseInfo.getPointerInfo().getLocation().y;
 
-        byte type = recivedBytes[0];
-        int value = (int) recivedBytes[1];
-        if (value == 0) {
-            try {
-                value = stream.read();
-            } catch (IOException ex) {
-                Logger.getLogger(MouseController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        switch (type) {
-            case UP:
-                y += (int) value * screenY / 40;
-                break;
-            case DOWN:
-                y -= (int) value * screenY / 40;
-                break;
-            case RIGHT:
-                x += (int) value * screenX / 40;
-                break;
-            case LEFT:
-                x -= (int) value * screenX / 40;
-                break;
-            case CENTER:
-                x = screenX / 2;
-                y = screenY / 2;
-                break;
-            default:
-                Notification.notify("unknown control byte");
-                break;
-        }
-        robot.mouseMove(x, y);
-    }
+		byte type = recivedBytes[0];
+		int value = (int) recivedBytes[1];
+		if (value == 0) {
+			try {
+				value = stream.read();
+			} catch (IOException ex) {
+				Logger.getLogger(MouseController.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+		}
+		switch (type) {
+		case UP:
+			mouseThread.setY(value);
+			break;
+		case DOWN:
+			mouseThread.setY(-value);
+			break;
+		case RIGHT:
+			mouseThread.setX(value);
+			break;
+		case LEFT:
+			mouseThread.setX(-value);
+			break;
+		case CENTER:
+			x = screenX / 2;
+			y = screenY / 2;
+			robot.mouseMove(x, y);
+			break;
+		default:
+			Notification.notify("unknown control byte");
+			break;
+		}
+	}
+
+	private class MouseThread extends Thread {
+		private Robot r;
+		private int xSpeed = 0, ySpeed = 0;
+
+		public MouseThread(Robot r) {
+			this.r = r;
+		}
+
+		public synchronized void setX(int x) {
+			xSpeed = x;
+		}
+
+		public synchronized void setY(int y) {
+			ySpeed = y;
+		}
+
+		public synchronized void stopMouse() {
+			xSpeed = 0;
+			ySpeed = 0;
+		}
+
+		@Override
+		public void run() {
+			int x, y;
+			while (true) {
+				x = MouseInfo.getPointerInfo().getLocation().x;
+				y = MouseInfo.getPointerInfo().getLocation().y;
+				r.mouseMove(x + xSpeed, y + ySpeed);
+			}
+		}
+	}
 }
