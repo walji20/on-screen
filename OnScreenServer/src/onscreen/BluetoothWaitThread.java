edc3714275df38bb8cc73da928c3ddc89343dd47
lundberg.mattias <lundberg.mattias@gmail.com>
2011-11-15 14:07:50 +0000

    are able to listen on bluetooth and lan at the same time

---
+++ OnScreenServer/src/onscreen/BluetoothWaitThread.java
@@ -0,0 +1,64 @@
+package onscreen;
+
+/**
+ *
+ * @author Mattias
+ */
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+
+public class BluetoothWaitThread implements Runnable {
+
+    @Override
+    public void run() {
+        waitForConnection();
+    }
+
+    private void waitForConnection() {
+        // retrieve the local Bluetooth device object
+        LocalDevice local = null;
+        StreamConnectionNotifier notifier;
+        StreamConnection connection = null;
+        try {
+            local = LocalDevice.getLocalDevice();
+        } catch (BluetoothStateException ex) {
+            Notification.notify("Could not initiate bluetooth..");
+            return;
+        }
+        
+        // setup the server to listen for connection
+        try {
+            local.setDiscoverable(DiscoveryAgent.GIAC);
+
+            Notification.notify(local.getBluetoothAddress() + "\n");
+
+            UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
+            String url = "btspp://localhost:" + uuid.toString()
+                    + ";name=OnScreen";
+            notifier = (StreamConnectionNotifier) Connector.open(url);
+        } catch (Exception e) {
+            return;
+        }
+
+        // waiting for connection
+        while (true) {
+            try {
+                connection = notifier.acceptAndOpen();
+                Thread processThread = new Thread(
+                        new ProcessConnectionThread(connection));
+                processThread.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+    }
+}
---
+++ OnScreenServer/src/onscreen/LanWaitThread.java
@@ -0,0 +1,38 @@
+package onscreen;
+
+/**
+ *
+ * @author Mattias
+ */
+import java.io.IOException;
+import javax.microedition.io.SocketConnection;
+import com.intel.bluetooth.gcf.socket.ServerSocketConnection;
+
+public class LanWaitThread implements Runnable {
+
+    @Override
+    public void run() {
+        waitForConnection();
+    }
+
+    private void waitForConnection() {
+
+        // Use a normal server socket connection
+        try {
+            ServerSocketConnection scn = new ServerSocketConnection(8633);
+            SocketConnection connection;
+            while (true) {
+                Notification.notify("waiting for connection...\n");
+                connection = (SocketConnection) scn.acceptAndOpen();
+                Notification.notify("got connection...\n");
+
+                Thread processThread = new Thread(
+                        new ProcessConnectionThread(connection));
+                processThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+}
