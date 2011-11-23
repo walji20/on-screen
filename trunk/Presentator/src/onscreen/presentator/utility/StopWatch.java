07bb4fefbce90da40fb1c54a2ecb832faba35fbe
kivie89@gmail.com <kivie89@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-23 14:51:38 +0000

    Fixed formatting

--- trunk/Presentator/src/onscreen/presentator/utility/StopWatch.java
+++ trunk/Presentator/src/onscreen/presentator/utility/StopWatch.java
@@ -3,19 +3,23 @@ package onscreen.presentator.utility;
 import android.os.SystemClock;
 import android.widget.Chronometer;
 
-public class StopWatch{
+public class StopWatch {
 	private Chronometer chrono;
 	private Long currentTimeLastStop;
-	private boolean isRunning=false;
-	private boolean clockReseted=true;
-	public enum WatchState {RUNNING, PAUSED, STOPPED};
+	private boolean isRunning = false;
+	private boolean clockReseted = true;
+
+	public enum WatchState {
+		RUNNING, PAUSED, STOPPED
+	};
+
 	private WatchState state = WatchState.STOPPED;
-	
-	public StopWatch(final Chronometer chrono){
-		this.chrono=chrono;
+
+	public StopWatch(final Chronometer chrono) {
+		this.chrono = chrono;
 		resetClock();
 	}
-	
+
 	/**
 	 * @return the displayed time in seconds
 	 */
@@ -26,66 +30,67 @@ public class StopWatch{
 			return (time() - chrono.getBase()) / 1000;
 		}
 	}
-	
+
 	/**
 	 * 
-	 * @param time the stopwatch is displayed
+	 * @param time
+	 *            the stopwatch is displayed
 	 */
-	public void setBaseTime(int time){
-		Long time2=time();
-		chrono.setBase(time2-time*1000);
-		clockReseted=false;
-		currentTimeLastStop=time2;
+	public void setBaseTime(int time) {
+		Long time2 = time();
+		chrono.setBase(time2 - time * 1000);
+		clockReseted = false;
+		currentTimeLastStop = time2;
 	}
 
 	/**
 	 * Start the clock to tick
 	 */
-	public void startClock(){
+	public void startClock() {
 		if (isRunning)
 			return;
-		
-		if(clockReseted){
+
+		if (clockReseted) {
 			chrono.setBase(time());
 		} else {
-			long time=getRestoreTime();
+			long time = getRestoreTime();
 			chrono.setBase(time);
 		}
-				
+
 		chrono.start();
-		isRunning=true;
-		clockReseted=false;
+		isRunning = true;
+		clockReseted = false;
 		state = WatchState.RUNNING;
 	}
-	
+
 	private long getRestoreTime() {
-		return chrono.getBase()+time()-currentTimeLastStop;
+		return chrono.getBase() + time() - currentTimeLastStop;
 	}
 
-	public void pauseClock(){
-		if (!isRunning){
+	public void pauseClock() {
+		if (!isRunning) {
 			return;
 		}
 		chrono.stop();
 		setClockIsNotRunning();
 		state = WatchState.PAUSED;
 	}
-	
-	public void resetClock(){
+
+	public void resetClock() {
 		chrono.setBase(time());
 		chrono.refreshDrawableState();
 		chrono.stop();
 		setClockIsNotRunning();
-		clockReseted=true;
+		clockReseted = true;
 		state = WatchState.STOPPED;
 	}
-	
+
 	private void setClockIsNotRunning(Long time) {
-		currentTimeLastStop=time;
-		isRunning=false;
+		currentTimeLastStop = time;
+		isRunning = false;
 	}
-	
-	private void setClockIsNotRunning(){
+
+	private void setClockIsNotRunning() {
 		setClockIsNotRunning(time());
 	}
 
