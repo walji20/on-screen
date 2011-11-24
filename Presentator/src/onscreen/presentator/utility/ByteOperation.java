e2a959c5917f69162969281856c901307bc84462
john.viklund <john.viklund@gmail.com>
2011-11-24 07:51:42 +0000

    Moved the byte operations to a new class

---
+++ Presentator/src/onscreen/presentator/utility/ByteOperation.java
@@ -0,0 +1,50 @@
+package onscreen.presentator.utility;
+
+public class ByteOperation {
+	public static final byte[] longToBytes(long v) {
+		byte[] writeBuffer = new byte[8];
+
+		writeBuffer[0] = (byte) (v >>> 56);
+		writeBuffer[1] = (byte) (v >>> 48);
+		writeBuffer[2] = (byte) (v >>> 40);
+		writeBuffer[3] = (byte) (v >>> 32);
+		writeBuffer[4] = (byte) (v >>> 24);
+		writeBuffer[5] = (byte) (v >>> 16);
+		writeBuffer[6] = (byte) (v >>> 8);
+		writeBuffer[7] = (byte) (v >>> 0);
+		return writeBuffer;
+	}
+
+	public static final byte[] intToBytes(int v) {
+		byte[] writeBuffer = new byte[4];
+
+		writeBuffer[0] = (byte) (v >>> 24);
+		writeBuffer[1] = (byte) (v >>> 16);
+		writeBuffer[2] = (byte) (v >>> 8);
+		writeBuffer[3] = (byte) (v >>> 0);
+
+		return writeBuffer;
+	}
+
+	public static final int bytesToInt(byte[] b) {
+		int i = 0;
+
+		i += unsignedByteToInt(b[0]) << 24;
+		i += unsignedByteToInt(b[1]) << 16;
+		i += unsignedByteToInt(b[2]) << 8;
+		i += unsignedByteToInt(b[3]);
+		return i;
+	}
+
+	private static int unsignedByteToInt(byte b) {
+		return (int) b & 0xFF;
+	}
+
+	public static final byte[] charArrayToBytes(char[] array) {
+		byte[] bytes = new byte[array.length];
+		for (int i = 0; i < array.length; i++) {
+			bytes[i] = (byte) array[i];
+		}
+		return bytes;
+	}
+}
