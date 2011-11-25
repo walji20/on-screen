c40b6a63b34e2d47c7eaa01d82f111f0b6c96ead
lundberg.mattias <lundberg.mattias@gmail.com>
2011-11-25 10:25:06 +0000

    some changes in retriving the bluetooth adapter for better debugging + better handeling of comand line arguments

--- OnScreenServer/src/onscreen/BluetoothWaitThread.java
+++ OnScreenServer/src/onscreen/BluetoothWaitThread.java
@@ -1,5 +1,6 @@
 package onscreen;
 
+import java.io.IOException;
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
@@ -32,22 +33,28 @@ public class BluetoothWaitThread implements Runnable {
         try {
             local = LocalDevice.getLocalDevice();
         } catch (BluetoothStateException ex) {
-            Notification.debugMessage("Could not initiate bluetooth..");
+            Notification.debugMessage("Could not initiate the bluetooth adapter..\n"
+                    + "Do you have bluetooth turned on? Please restart application if you want to"
+                    + "use bluetooth");
             return;
         }
+        try {
+            local.setDiscoverable(DiscoveryAgent.GIAC);
+        } catch (BluetoothStateException ex) {
+            Notification.debugMessage("The bluetooth adapter could not be set discoverable.");
+        }
+
+        Notification.debugMessage("Local bluetooth address: " + local.getBluetoothAddress() + "\n");
+
+        UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
+        String url = "btspp://localhost:" + uuid.toString()
+                + ";name=OnScreen";
 
         // setup the server to listen for connection
         try {
-            local.setDiscoverable(DiscoveryAgent.GIAC);
-
-            Notification.debugMessage("Local bluetooth address: " + local.getBluetoothAddress() + "\n");
-
-            UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
-            String url = "btspp://localhost:" + uuid.toString()
-                    + ";name=OnScreen";
             notifier = (StreamConnectionNotifier) Connector.open(url);
-        } catch (Exception e) {
-            Notification.debugMessage("Problem starting bluetooth.");
+        } catch (IOException e) {
+            Notification.debugMessage("Problem opening bluetooth stream.");
             return;
         }
 
@@ -62,7 +69,7 @@ public class BluetoothWaitThread implements Runnable {
                 // try to hande problems with bluetooth waiting. 
                 Notification.debugMessage("Problem while waiting for connection. Restarting!");
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (InterruptedException ex) {
                     return;
                 }
