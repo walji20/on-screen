a71d3107afd18ee283b925eca3a34cc5efd6aa3a
lundberg.mattias@gmail.com <lundberg.mattias@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-25 10:21:50 +0000

    some changes in retriving the bluetooth adapter for better debugging + better handeling of comand line arguments

--- trunk/OnScreenServer/src/onscreen/OnScreen.java
+++ trunk/OnScreenServer/src/onscreen/OnScreen.java
@@ -15,11 +15,13 @@ public class OnScreen {
     static final String sumatraSettings = " -esc-to-exit -page 1 -presentation ";
     static final String env32 = System.getenv("PROGRAMFILES");
     static final String env64 = System.getenv("PROGRAMFILES(X86)");
-    static final String BTOFFSETTING = "nobt";
-    static final String LANOFFSETTING = "nolan";
+    static final String BTOFFSETTING = "--nobt";
+    static final String LANOFFSETTING = "--nolan";
+    static final String DEBUGSETTING = "--debug";
+    static final String PDFSETTING = "--pdf=";
     static boolean BT = true;
     static boolean LAN = true;
-    public static String pdfReader;
+    public static String pdfReader = "";
     static KeyController keyController;
 
     /**
@@ -27,33 +29,38 @@ public class OnScreen {
      * 
      * @param args the command line arguments
      */
-    // TODO: Linux bluetooth
-    // TODO: Better reading of arguments
     public static void main(String[] args) {
         keyController = new KeyController();
 
         if (args.length > 0) {
-            pdfReader = "";
             for (int i = 0; i < args.length; i++) {
-                final String DEBUGSETTING = "debug";
-
                 if (args[i].equals(DEBUGSETTING)) {
                     Notification.setDebug(true);
                 } else if (args[i].equals(BTOFFSETTING)) {
                     BT = false;
                 } else if (args[i].equals(LANOFFSETTING)) {
                     LAN = false;
+                } else if (args[i].startsWith(PDFSETTING)) {
+                    pdfReader = args[i].split("=")[1];
                 } else {
-                    pdfReader += args[i];
+                    Notification.debugMessage("Unknown setting " + args[i]);
                 }
             }
-        } else if ((new File(env32 + nativeSumatra)).exists()) {
-            pdfReader = env32 + nativeSumatra + sumatraSettings;
-        } else if ((new File(env64 + nativeSumatra)).exists()) {
-            pdfReader = env64 + nativeSumatra + sumatraSettings;
-        } else if ((new File(env32 + onScreenSumatra)).exists()) {
-            pdfReader = env32 + onScreenSumatra + sumatraSettings;
-        } else {
+        }
+
+        // If no arguments for pdf reader sent using sumatra, either native or 
+        // the one bundled with the onscreen installer
+        if (pdfReader.isEmpty()) {
+            if ((new File(env32 + nativeSumatra)).exists()) {
+                pdfReader = env32 + nativeSumatra + sumatraSettings;
+            } else if ((new File(env64 + nativeSumatra)).exists()) {
+                pdfReader = env64 + nativeSumatra + sumatraSettings;
+            } else if ((new File(env32 + onScreenSumatra)).exists()) {
+                pdfReader = env32 + onScreenSumatra + sumatraSettings;
+            }
+        }
+
+        if (pdfReader.isEmpty()) {
             Notification.debugMessage("Could not find pdf reader!");
         }
 
