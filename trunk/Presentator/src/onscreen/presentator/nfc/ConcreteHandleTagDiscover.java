110281f3cb28f00812538a920428068dd78697ca
vkg.lindgren@gmail.com <vkg.lindgren@gmail.com@a1afaa04-ddfc-90de-a81f-ff0826ec84d4>
2011-11-28 10:50:54 +0000

    More comments and name change.

--- trunk/Presentator/src/onscreen/presentator/nfc/ConcreteHandleTagDiscover.java
+++ trunk/Presentator/src/onscreen/presentator/nfc/ConcreteHandleTagDiscover.java
@@ -2,20 +2,29 @@ package onscreen.presentator.nfc;
 
 import java.util.Observable;
 
+/**
+ * The Concreate Handle Tag Discover which is observable and notify observers if change so they can get tag text.
+ * @author viktor
+ *
+ */
 public class ConcreteHandleTagDiscover extends Observable implements
 		HandleTagDiscover {
 	private String text = ":(";
 
-	public ConcreteHandleTagDiscover() {
-	}
-
+	/**
+	 * @param text discovered text on the tag.
+	 */
 	public void handleTagDiscover(String text) {
 		this.text = text;
 		setChanged();
 		notifyObservers();
 	}
 
-	public String getTag() {
+	/**
+	 * 
+	 * @return text found on tag last time discovered.
+	 */
+	public String getTagText() {
 		return text;
 	}
 
--- trunk/Presentator/src/onscreen/presentator/nfc/HandleTagDiscover.java
+++ trunk/Presentator/src/onscreen/presentator/nfc/HandleTagDiscover.java
@@ -1,5 +1,18 @@
 package onscreen.presentator.nfc;
 
+/**
+ * Interface that is used for handling when a new tag is discover and the
+ * handleTagDiscover should be called.
+ * 
+ * @author viktor
+ * 
+ */
+
 public interface HandleTagDiscover {
+
+	/**
+	 * Call this function when a tag is discovered.
+	 * @param text that was found on the tag.
+	 */
 	public void handleTagDiscover(String text);
 }
